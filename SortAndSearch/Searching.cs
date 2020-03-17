fe946a82cbef59848621707ba38104ec9c8a6ba2
Nico Bohn <n.bohn97@gmail.com>
2020-03-17 19:13:20 +0100

    Implemented Binary Search

--- SortAndSearch/Searching.cs
+++ SortAndSearch/Searching.cs
@@ -16,7 +16,7 @@ namespace SortAndSearch
             {
                 if (num == intSearchedFor)
                 {
-                    return $"Integer {intSearchedFor} found at index {index}";
+                    return $"Integer {intSearchedFor} first found at index {index}";
                 }
                 ++index;
             }
@@ -25,7 +25,7 @@ namespace SortAndSearch
         }
         
         // SequentialSearch With Strings
-        public static string SequentialSearchString(List<string> sortedList, string stringSearchedFor)
+        public static string SequentialSearch(List<string> sortedList, string stringSearchedFor)
         {
 
             long index = 0;
@@ -34,7 +34,7 @@ namespace SortAndSearch
             {
                 if (word == stringSearchedFor)
                 {
-                    return $"Integer {stringSearchedFor} found at index {index}";
+                    return $"String {stringSearchedFor} first found at index {index}";
                 }
                 ++index;
             }
@@ -43,31 +43,70 @@ namespace SortAndSearch
         
         
         
-        public static string BinarySearch(List<long> sortedList)
+        public static string BinarySearch(List<long> sortedList, long intSearchedFor)
         {
-            
-            
-            
+            int upperBound = sortedList.Count - 1;
+            int lowerBound = 0;
+            int location;
+            bool found = false;
 
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+            while (upperBound >= lowerBound)
+            {
+                location = (lowerBound + upperBound) / 2;
 
-            return "";
+                if (sortedList[location] == intSearchedFor)
+                {
+                    return $"{intSearchedFor} first found at index {location}";
+                }
+
+                else if(intSearchedFor > sortedList[location])
+                {
+                    lowerBound = location + 1;
+                }
+                else
+                {
+                    upperBound = location - 1;
+                }
+                
+            }
             
+            return $"{intSearchedFor} not found in List";
+  
+                
         }
+            
+            
+        
 
 
-        public static string BinarySearchString(List<String> sortedList)
+        public static string BinarySearch(List<String> sortedList, string stringSearchedFor)
         {
-            return "";
+            int upperBound = sortedList.Count - 1;
+            int lowerBound = 0;
+            int location;
+            bool found = false;
+
+            while (upperBound >= lowerBound)
+            {
+                location = (lowerBound + upperBound) / 2;
+
+                if (sortedList[location].Equals(stringSearchedFor))
+                {
+                    return $"{stringSearchedFor.ToUpper()} first found at index {location}";
+                }
+
+                else if(String.Compare(stringSearchedFor, sortedList[location], StringComparison.Ordinal) > 0)
+                {
+                    lowerBound = location + 1;
+                }
+                else if (String.Compare(stringSearchedFor, sortedList[location], StringComparison.Ordinal) < 0)
+                {
+                    upperBound = location - 1;
+                }
+                
+            }
+            
+            return $"{stringSearchedFor} not found in List";
         }
         
         
