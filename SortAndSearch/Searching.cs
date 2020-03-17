8c411ddd4893ed3d3b99ae1a72e066620ca504ba
Nico Bohn <n.bohn97@gmail.com>
2020-03-17 06:56:50 +0100

    Added Sequential Search for Int and String

--- SortAndSearch/Searching.cs
+++ SortAndSearch/Searching.cs
@@ -1,7 +1,56 @@
-﻿namespace SortAndSearch
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortAndSearch
 {
-    public class Searching
+    public static class Searching
     {
+
+        // SequentialSearch With Integers
+        public static string SequentialSearch(List<int> sortedList, int x)
+        {
+
+
+            int searchInt = x;
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                if (sortedList[i] == searchInt)
+                {
+                    return $"Integer {searchInt} found at index {i}";
+                }
+            }
+            return $"Integer {searchInt} not found in List.";
+
+        }
+        
+        // SequentialSearch With Strings
+        public static string SequentialSearch(List<string> sortedList, string x)
+        {
+            
+            string searchString = x;
+
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                if (sortedList[i] == searchString)
+                {
+                    return $"String {searchString} found at index {i}";
+                }
+            }
+            return $"String {searchString} not found in List.";
+
+        }
+        
+        
+        
+        public static int BinarySearch(List<int> sortedList)
+        {
+            int x = 0;
+
+
+            return x;
+            
+        }
         
     }
 }
