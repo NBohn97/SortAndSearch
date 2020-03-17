3b6053de9d1bd134aaff8a5588cca984448efa7d
Nico Bohn <n.bohn97@gmail.com>
2020-03-17 06:56:19 +0100

    Added Stopwatch Functions

--- SortAndSearch/Performance.cs
+++ SortAndSearch/Performance.cs
@@ -1,17 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SortAndSearch
 {
-    public class Performance
+    public static class Performance
     {
 
-        static void TimeFunction(Func<List<int>,List<int>> myFunction)
+        // Uses a Sort Function and A List as Input. Returns sorted List and returns time elapsed on function
+        public static List<int> ElapsedTimeSorting(Func<List<int>,List<int>> mySortFunction, List<int> x)
         {
-            // Implement Stopwatch
             
-            
+            var sw = new Stopwatch();
+            sw.Start();
+            List<int> sortedList = mySortFunction(x);
+            sw.Stop();
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            return sortedList;
         }
+
+        public static string ElapsedTimeSearching(Func<List<int>, int, string> mySearchFunction, List<int> sortedList, int searchingFor)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            string result = mySearchFunction(sortedList,searchingFor);
+            sw.Stop();
+            Console.WriteLine($"Time elapsed: {sw.Elapsed}");
+            return result;
+        }
+        
         
         
     }
