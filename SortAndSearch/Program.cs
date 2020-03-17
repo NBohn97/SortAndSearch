5745af5e7ea072ac32ad998f5c1e0cbf696ad1e2
Nico Bohn <n.bohn97@gmail.com>
2020-03-17 19:13:08 +0100

    Refactored Main

--- SortAndSearch/Program.cs
+++ SortAndSearch/Program.cs
@@ -13,7 +13,45 @@ namespace SortAndSearch
 
         static void Main(string[] args)
         {
+
+            bool restart = true;
             
+            while(restart == true){
+                int firstChoice = ChooseFirst();
+                ChooseSecond(firstChoice);
+
+                Console.WriteLine("Restart? (y)es (n)o:");
+                string x = Console.ReadLine();
+                restart = x == "y";
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             /* SORT MAIN
             
             // Generate a random List
@@ -35,8 +73,8 @@ namespace SortAndSearch
             Console.WriteLine($"Is Bogo Sorted List sorted ?: {IsSorted(sortedBogo)}\n");
 
             */
-            
-            
+
+
 
             // SEARCH MAIN
 
@@ -50,21 +88,33 @@ namespace SortAndSearch
 
 
 
-            // SEARCH CITY SORTED SEQUENTIAL STRING
+
+
+
+
+
+
+
+
+
+
+
             /*
+
+            // SEARCH CITY SORTED SEQUENTIAL STRING
             Console.WriteLine("Which City are you searching for? :");
             string searchingForString = Console.ReadLine();
             
             var convList = TxtToStringList("C:\\Solutions\\SortThisShit\\SortAndSearch\\CitiesSorted.txt");
-            Console.WriteLine(ElapsedTimeSearchingString(SequentialSearchString,convList,searchingForString));
-            */
-
+            Console.WriteLine(ElapsedTimeSearchingString(SequentialSearch,convList,searchingForString));
+            
+            // Search long list SEQUENTIAL
             List<long> generatedList = CreateSortedNumberList();
             PrintList(generatedList);
             
+            Console.WriteLine(ElapsedTimeSearching(SequentialSearch,generatedList,50));
 
-
-
+            */
 
 
 
@@ -102,5 +152,123 @@ namespace SortAndSearch
 
 
         }
+
+        public static int ChooseFirst()
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Choose:");
+            int selection = 0;
+            
+            while (selection != 1 && selection != 2)
+            {
+                Console.WriteLine("(1) Sort");
+                Console.WriteLine("(2) Search");
+
+                selection = int.Parse(Console.ReadLine());
+
+            }
+            
+            return selection;
+        }
+        
+        
+        
+        public static void ChooseSecond(int selection1)
+        {
+
+            int selection2;
+            int selection3;
+            string selectionSort1;
+            string selectionSort2;
+            
+            switch (selection1)
+            {
+                case 1:
+                    Console.WriteLine("Enter Amount of Numbers in random List:");
+                    List<long> generatedRandomList = CreateRandomNumberList();
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Choose Sorting Algorithm 1");
+                    Console.WriteLine("(1) BubbleSort");
+                    Console.WriteLine("(2) BubbleSort Optimized");
+                    Console.WriteLine("(3) BogoSort");
+                    selection2 = int.Parse(Console.ReadLine());
+                    switch (selection2)
+                    {
+                        case 1:
+                            var sortedBubble = ElapsedTimeSorting(BubbleSort,generatedRandomList);
+                            break;
+                        case 2:
+                            var sortedBubbleOptimized = ElapsedTimeSorting(BubbleSortOptimized,generatedRandomList);
+                            break;
+                        case 3:
+                            var sortedBogoSort = ElapsedTimeSorting(BogoSort,generatedRandomList);
+                            break;
+                    }
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine("Choose Sorting Algorithm 2");
+                    Console.WriteLine("(1) BubbleSort");
+                    Console.WriteLine("(2) BubbleSort Optimized");
+                    Console.WriteLine("(3) BogoSort");
+                    selection2 = int.Parse(Console.ReadLine());
+                    switch (selection2)
+                    {
+                        case 1:
+                            var sortedBubble = ElapsedTimeSorting(BubbleSort,generatedRandomList);
+                            break;
+                        case 2:
+                            var sortedBubbleOptimized = ElapsedTimeSorting(BubbleSortOptimized,generatedRandomList);
+                            break;
+                        case 3:
+                            var sortedBogoSort = ElapsedTimeSorting(BogoSort,generatedRandomList);
+                            break;
+                    }
+
+
+                    break;
+                
+                case 2:
+                    Console.WriteLine("Search Integers or String?");
+                    Console.WriteLine("(1) Integer ");
+                    Console.WriteLine("(2) String ");
+                    selection2 = int.Parse(Console.ReadLine());
+                    switch (selection2)
+                    {
+                        case 1:
+                            // need to randomize ascending list from CreateSortedNumbersList();
+                            List<long> generatedList = CreateSortedNumberList();
+                            Console.WriteLine("Enter Integer you are searching for");
+                            int searchingFor = int.Parse(Console.ReadLine());
+                            
+                            Console.WriteLine("------------------------------");
+                            Console.WriteLine("Sequential Search: ");
+                            Console.WriteLine(ElapsedTimeSearching(SequentialSearch,generatedList,searchingFor));
+                            Console.WriteLine("Binary Search: ");
+                            Console.WriteLine(ElapsedTimeSearching(BinarySearch,generatedList,searchingFor));
+                            break;
+                        case 2:
+                            Console.WriteLine("Which City are you searching for? :");
+                            string searchingForString = Console.ReadLine();
+                            // make path relative
+                            var convList = TxtToStringList("C:\\Solutions\\SortThisShit\\SortAndSearch\\CitiesSorted.txt");
+                            Console.WriteLine("Sequential Search:");
+                            Console.WriteLine(ElapsedTimeSearchingString(SequentialSearch,convList,searchingForString));
+                            Console.WriteLine(ElapsedTimeSearchingString(BinarySearch,convList,searchingForString));
+                            break;
+                    }
+                    
+                    break;
+                
+            }
+            
+    
+    
+    
+        }
+        
     }
+    
+
+    
+    
+    
 }
