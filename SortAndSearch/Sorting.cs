6008a595af335ec771e5c595726539efca58caaa
Nico Bohn <n.bohn97@gmail.com>
2020-03-17 06:57:31 +0100

    Fixed Random List being overwritten

--- SortAndSearch/Sorting.cs
+++ SortAndSearch/Sorting.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SortAndSearch
 {
@@ -18,43 +19,96 @@ namespace SortAndSearch
         // Uses Bubble Sort to sort a List
         public static List<int> BubbleSort(List<int> unsortedList)
         {
-            var x = unsortedList.Count;
+            var listCopy = new List<int>(unsortedList);
+            var x = listCopy.Count;
             for (var i = 0; i < x - 1; i++)
             for (var j = 0; j < x - i - 1; j++)
-                if (unsortedList[j] > unsortedList[j + 1])
+                if (listCopy[j] > listCopy[j + 1])
                 {
-                    var temp = unsortedList[j];
-                    unsortedList[j] = unsortedList[j + 1];
-                    unsortedList[j + 1] = temp;
+                    var temp = listCopy[j];
+                    listCopy[j] = listCopy[j + 1];
+                    listCopy[j + 1] = temp;
                 }
 
-            return unsortedList;
+            return listCopy;
         }
         
         // Optimized Bubble Sort to sort a list
         public static List<int> BubbleSortOptimized(List<int> unsortedList)
         {
-            var x = unsortedList.Count;
+            var listCopy = new List<int>(unsortedList);
+            var x = listCopy.Count;
             bool swap;
             for (var i = 0; i < x - 1; i++)
             {
                 swap = false;
                 for (var j = 0; j < x - i - 1; j++)
-                    if (unsortedList[j] > unsortedList[j + 1])
+                    if (listCopy[j] > listCopy[j + 1])
                     {
-                        var temp = unsortedList[j];
-                        unsortedList[j] = unsortedList[j + 1];
-                        unsortedList[j + 1] = temp;
+                        var temp = listCopy[j];
+                        listCopy[j] = listCopy[j + 1];
+                        listCopy[j + 1] = temp;
                         swap = true;
                     }
 
                 if (swap == false) break;
             }
 
-            return unsortedList;
+            return listCopy;
+        }
+        
+        public static List<int> BogoSort(List<int> unsortedList)
+        {
+            var listCopy = new List<int>(unsortedList);
+            var rand = new Random();
+            var n = listCopy.Count;
+            
+            while (!IsSorted(listCopy))
+            {
+                for (var i = listCopy.Count - 1; i > 1; i--)
+                {
+                    var random = rand.Next(i + 1);
+                    var value = listCopy[random];
+                    listCopy[random] = listCopy[i];
+                    listCopy[i] = value;
+                }
+                
+            }
+            
+            return listCopy;
         }
         
         
+
+        public static List<int> BogoSortWithLog(List<int> unsortedList)
+        {
+            var listCopy = new List<int>(unsortedList);
+            var rand = new Random();
+            var n = listCopy.Count;
+
+            var shuffled = 0;
+
+            while (!IsSorted(listCopy))
+            {
+                for (var i = listCopy.Count - 1; i > 1; i--)
+                {
+                    var random = rand.Next(i + 1);
+                    var value = listCopy[random];
+                    listCopy[random] = listCopy[i];
+                    listCopy[i] = value;
+                }
+
+                ++shuffled;
+                if (shuffled % 1000 == 0)
+                {
+                    Console.WriteLine($"Shuffled {shuffled} times.");
+                }
+                
+            }
+            
+            Console.WriteLine($"Shuffled {shuffled} times.");
+            return listCopy;
+        }
         
         
         
